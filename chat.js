bbbc589bb0bd9b23467023c4249d72fab187d2dd
Vlad Ermolaev <Lacre24@gmail.com>
2017-02-27 16:16:39 +0300

    база подключена + пример на передачу в базу

--- chat.js
+++ chat.js
@@ -8,7 +8,21 @@ var port = 3000; // Можно любой другой порт
 logger.debug('Script has been started...'); // Логгируем.
 server.listen(port); // Теперь мы можем подключиться к нашему серверу через localhost:3000 при запущенном скрипте
 app.use(express.static(__dirname + '/public'));
+//подключение базы
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', function (err) {
+    logger.error('connection error:', err.message);
+});
+db.once('open', function callback () {
+    logger.info("Connected to DB!");
+});
 
+var userSchema = mongoose.Schema({
+    name: String
+});
+var User = mongoose.model('User', userSchema);
 
 io.on('connection', function (socket) {
     var name = 'U' + (socket.id).toString().substr(1, 4);
@@ -20,8 +34,16 @@ io.on('connection', function (socket) {
 
     socket.on('message', function (msg) { // Обработчик на событие 'message' и аргументом (msg) из переменной message
         logger.warn('-----------'); // Logging
-        logger.warn('User: ' + name + ' | Message: ' + msg[0] + '| TextArea:' + msg[1]);
+        logger.warn('User: ' + name + ' | Message: ' + msg[0] + ' | TextArea: ' + msg[1]);
         logger.warn('====> Sending message to other chaters...');
         io.sockets.emit('messageToClients', msg, name); // Отправляем всем сокетам событие 'messageToClients' и отправляем туда же два аргумента (текст, имя юзера)
     });
+
+    socket.on('userNameMongo', function(userNameMongo) {
+        var userMongo = new User({ name: userNameMongo });
+        logger.warn("name:"+ userMongo.name);
+    });
+
+
+
 });
--- public/main.js
+++ public/main.js
@@ -1,30 +1,40 @@
 var port = 3000; // Указываем порт на котором у на стоит сокет
 var socket = io.connect('http://localhost:' + port); // Тут мы объявляем "socket" (дальше мы будем с ним работать) и подключаемся сразу к серверу через порт
 var textareaID = 'general';
+
+
+
 socket.on('userName', function (userName) { // Создаем прослушку 'userName' и принимаем переменную name в виде аргумента 'userName'
     console.log('You\'r username is => ' + userName); // Логгирование в консоль браузера
-    $('#'+textareaID).val($('#'+textareaID).val() + 'You\'r username => ' + userName + '\n'); // Выводим в поле для текста оповещение для подключенного с его ником
+    $('#' + textareaID).val($('#' + textareaID).val() + 'You\'r username => ' + userName + '\n'); // Выводим в поле для текста оповещение для подключенного с его ником
 });
 
 socket.on('newUser', function (userName) { // Думаю тут понятно уже =)
     console.log('New user has been connected to chat | ' + userName); // Логгирование
-    $('#'+textareaID).val($('#'+textareaID).val() + userName + ' connected!\n'); // Это событие было отправлено всем кроме только подключенного, по этому мы пишем другим юзерам в поле что 'подключен новый юзер' с его ником
+    $('#' + textareaID).val($('#' + textareaID).val() + userName + ' connected!\n'); // Это событие было отправлено всем кроме только подключенного, по этому мы пишем другим юзерам в поле что 'подключен новый юзер' с его ником
 });
 
-$(document).on('click', 'button', function () { // Прослушка кнопки на клик
+$(document).on('click', '#sendMsg', function () { // Прослушка кнопки на клик
     var message = [];
-     message[0] = $('input').val(); // Все что в поле для ввода записываем в переменную
-     message[1] = textareaID;
+    message[0] = $('input').val(); // Все что в поле для ввода записываем в переменную
+    message[1] = textareaID;
     socket.emit('message', message); // Отправляем событие 'message' на сервер c самим текстом (message)- как переменная
     $('input').val(null); // Заполняем поле для ввода 'пустотой'
 });
 
+$(document).on('click', '#regbtn', function() {
+    var userNameMongo = $('#userName').val();
+    console.log(userNameMongo);
+    socket.emit('userNameMongo',userNameMongo); 
+    $('#userName').val(null); 
+})
+
 socket.on('messageToClients', function (msg, name) {
-    console.log(name + ' | => ' + msg); // Логгирование в консоль браузера
-    $('#'+textareaID).val($('#'+textareaID).val() + name + ' : ' + msg[0] + '\n'); // Добавляем в поле для текста сообщение типа (Ник : текст)
+    console.log(name + ' | => ' + 'Message: ' + msg[0] + ' | TextArea: ' + msg[1]); // Логгирование в консоль браузера
+    $('#' + msg[1]).val($('#' + msg[1]).val() + name + ' : ' + msg[0] + '\n'); // Добавляем в поле для текста сообщение типа (Ник : текст)
 });
 
-document.onclick = function(e) {
+$(document).on('click', function (e) {
     e = e || window.event;
     var el = e.target || e.srcElement;
     if (el.nodeName == 'TEXTAREA') {
@@ -32,7 +42,7 @@ document.onclick = function(e) {
     } else {
         return null;
     }
-}
+});
 
 
 
